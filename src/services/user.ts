026174fd01b146fc97df534612ad309bbc1246c2
Chirag <chirgjin@gmail.com>
2022-08-18 20:46:56 +0530

    feat: service for user crud

---
+++ src/services/user.ts
@@ -0,0 +1,45 @@
+import argon2 from 'argon2'
+
+import { User, userRepository } from 'src/models'
+
+export async function createUser({
+  name,
+  email,
+  password,
+}: Pick<User, 'name' | 'email' | 'password'>) {
+  const userEmail = email.toLowerCase()
+
+  if (
+    (await userRepository.search().where('email').equals(userEmail).count()) > 0
+  ) {
+    throw new Error('Email already exists')
+  }
+
+  const user = await userRepository.createAndSave({
+    name,
+    email: userEmail,
+    password: await argon2.hash(password),
+    apiKey: await User.generateApiKey(),
+    createdAt: new Date(),
+  })
+
+  return user
+}
+
+export async function updateUser({
+  name,
+  password,
+  user,
+}: { user: User } & Partial<Pick<User, 'name' | 'password'>>) {
+  if (name) {
+    user.name = name
+  }
+
+  if (password) {
+    user.password = await argon2.hash(password)
+  }
+
+  user.updatedAt = new Date()
+
+  await userRepository.save(user)
+}
