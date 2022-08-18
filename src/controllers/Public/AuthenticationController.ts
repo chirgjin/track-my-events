c8a7420bf437e0e6d6d58b5236982f420e63e5cf
Chirag <chirgjin@gmail.com>
2022-08-18 23:37:08 +0530

    feat: login API

---
+++ src/controllers/Public/AuthenticationController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express'
+
+import BadRequestException from 'src/exceptions/BadRequestException'
+import { refreshTokenRepository, userRepository } from 'src/models'
+import { createAccessToken, createRefreshToken } from 'src/services'
+import { StringField, validate } from 'src/validator'
+
+export default class {
+  public async login(req: Request, res: Response) {
+    const body = await validate(req.body, {
+      email: new StringField({ email: true, maxLength: 256, minLength: 3 }),
+      password: new StringField({}),
+    })
+
+    const user = await userRepository
+      .search()
+      .where('email')
+      .equals(body.email.toLowerCase())
+      .first()
+
+    if (!user) {
+      throw new BadRequestException({
+        email: "User with this email doesn't exist",
+      })
+    }
+
+    if (!(await user.verifyPassword(body.password))) {
+      throw new BadRequestException({
+        password: 'Incorrect password',
+      })
+    }
+
+    const accessToken = await createAccessToken({ user })
+    const refreshToken = await createRefreshToken({ user })
+
+    return res.success(
+      {
+        user,
+        accessToken: accessToken.token,
+        refreshToken: refreshToken.token,
+      },
+      201
+    )
+  }
+
+  public async refreshToken(req: Request, res: Response) {
+    const body = await validate(req.body, {
+      refreshToken: new StringField({}),
+    })
+
+    const refreshToken = await refreshTokenRepository
+      .search()
+      .where('token')
+      .equals(body.refreshToken)
+      .first()
+
+    if (!refreshToken) {
+      throw new BadRequestException({
+        refreshToken: 'Invalid refresh token',
+      })
+    }
+
+    const user = await userRepository.fetch(refreshToken.userId)
+
+    const accessToken = await createAccessToken({
+      user,
+    })
+    const newRefreshToken = await createRefreshToken({ user })
+
+    await refreshTokenRepository.remove(refreshToken.entityId)
+
+    return res.success(
+      {
+        accessToken: accessToken.token,
+        refreshToken: newRefreshToken.token,
+        user,
+      },
+      200
+    )
+  }
+}
---
+++ src/routes/v1/public.ts
@@ -0,0 +1,15 @@
+import { Router } from 'src/helpers'
+import AuthenticationMiddleware from 'src/middlewares/AuthenticationMiddleware'
+
+export const router = new Router()
+
+router.post('/users/', 'Public/UsersController.create')
+router
+  .get('/users/me/', 'Public/UsersController.me')
+  .middleware(AuthenticationMiddleware)
+
+router.post('/auth/login/', 'Public/AuthenticationController.login')
+router.post(
+  '/auth/refresh-token/',
+  'Public/AuthenticationController.refreshToken'
+)
