207b540b17d514891ec9fb7d56269376758c1a37
Chirag <chirgjin@gmail.com>
2022-08-21 22:46:06 +0530

    fix: add route for update user API

--- services/user-service/src/routes/v1/public.ts
+++ services/user-service/src/routes/v1/public.ts
@@ -7,6 +7,9 @@ router.post('/users/', 'Public/UsersController.create')
 router
   .get('/users/me/', 'Public/UsersController.me')
   .middleware(AuthenticationMiddleware)
+router
+  .put('/users/me/', 'Public/UsersController.update')
+  .middleware(AuthenticationMiddleware)
 
 router.post('/auth/login/', 'Public/AuthenticationController.login')
 router.post(
